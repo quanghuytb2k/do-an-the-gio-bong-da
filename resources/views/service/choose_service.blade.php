b3452ab64d224f9cf1c50ddde2d7b683554b7532
sytruong2001 <vanyyy2001@gmail.com>
2023-12-26 22:40:54 +0700

    sửa giao diện trang chọn dịch vụ

--- resources/views/service/choose_service.blade.php
+++ resources/views/service/choose_service.blade.php
@@ -1,24 +1,41 @@
 @extends('layouts.app')
 <style>
-    .service-card {
+    /* .service-card {
         background-color: #0d6efd;
         color: #fff;
         cursor: pointer;
         border-radius: 1rem;
         box-shadow: 10px 5px 5px gray;
-    }
+    } */
 
-    .service-card:hover {
+    /* .service-card:hover {
         background-color: #fff;
         color: #0d6efd;
         border: 1px solid #0d6efd;
+    } */
+    .service-content{
+        background: white;
+        box-shadow: 5px 10px 20px gray;
+        border-radius: 20px;
+        height: 100% !important;
+        padding: 20px;
+        cursor: pointer;
+    }
+    .service-content .service-title {
+        padding: 5px 10px;
+        font-weight: 500;
+        border-radius: 10px;
+    }
+    .service-card a, .service-card a:hover{
+        text-decoration: none;
+        color: #000000;
     }
 </style>
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-lg-12">
-            <h2>Chọn dịch vụ</h2>
+        <h2>Chọn dịch vụ</h2>
+        {{-- <div class="col-lg-12">
             <div class="d-flex justify-content-around align-item-center">
                 @foreach($services as $service)
                 <a href="{{route('choose-service-pack', $service->id)}}">
@@ -30,7 +47,27 @@
                 @endforeach
             </div>
 
+        </div> --}}
+    </div>
+    <div class="row pt-4">
+        @php
+            $index = 0;
+        @endphp
+        @foreach($services as $service)
+        @php
+            $index ++;
+        @endphp
+        <div class="col-lg-4 service-card mt-3">
+            <a href="{{route('choose-service-pack', $service->id)}}">
+                <div class="service-content">
+                    <span class="service-title {{ $index % 3 == 0 ? 'bg-info text-primary' : ($index % 2 == 0 ? 'bg-success' : ($index % 1 == 0 ? 'bg-warning text-danger' : ''))}}">
+                        {{$service->name}}
+                    </span>
+                    <p class="mt-2">{{$service->description}}</p>
+                </div>
+            </a>
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
--- resources/views/service/choose_service_pack.blade.php
+++ resources/views/service/choose_service_pack.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 <style>
-    .service-card {
+    /* .service-card {
         background-color: #fff;
         color: #0d6efd;
         border: 1px solid #0d6efd;
@@ -18,13 +18,40 @@
 
     h3{
         text-decoration: none !important;
+    } */
+    .service-content{
+        background: white;
+        box-shadow: 5px 10px 20px gray;
+        border-radius: 20px;
+        height: 100% !important;
+        padding: 20px;
+        cursor: pointer;
+    }
+    .service-content .service-title {
+        padding: 5px 10px;
+        font-weight: 500;
+        border-radius: 10px;
+    }
+    .service-content .fa-check{
+        font-weight: 400;
+    }
+    .service-content button, .service-content button:hover {
+        text-align: center;
+        border: none;
+        outline: none;
+        padding: 5px 10px;
+        border-radius: 10px;
+    }
+    .service-card a, .service-card a:hover{
+        text-decoration: none;
+        color: #000000;
     }
 </style>
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-lg-12">
-            <h2>Chọn gói dịch vụ</h2>
+        <h2>Chọn gói dịch vụ</h2>
+        {{-- <div class="col-lg-12">
             <div class="d-flex justify-content-around align-item-center">
                 @foreach($services as $service)
                 <a href="{{route('choose-service-pack/next', $service->id)}}">
@@ -42,7 +69,41 @@
                 @endforeach
             </div>
 
+        </div> --}}
+    </div>
+    <div class="row pt-4">
+        @php
+            $index = 0;
+        @endphp
+        @foreach($services as $service)
+        @php
+            $index ++;
+        @endphp
+        <div class="col-lg-4 service-card mt-3">
+            <a href="{{route('choose-service-pack/next', $service->id)}}">
+                <div class="service-content">
+                    <span class="service-title {{ $index % 3 == 0 ? 'bg-info text-primary' : ($index % 2 == 0 ? 'bg-success' : ($index % 1 == 0 ? 'bg-warning text-danger' : ''))}}">
+                        {{$service->name}}
+                    </span>
+                    <p class="mt-2">{{$service->service->description}}</p>
+                    @if($service->price == 0)
+                        <h3>Miễn phí</h3>
+                    @else
+                        <h3>@convert($service->price) đ / tháng</h3>
+                    @endif
+                    <div class="py-2">
+                        <i class="fa fa-check"></i> Không giới hạn tính năng
+                    </div>
+                    <div class="py-2">
+                        <i class="fa fa-check"></i> Không phí khởi tạo
+                    </div>
+                    <div class="d-flex justify-content-center pt-3">
+                        <button class="text-white {{ $index % 3 == 0 ? 'bg-info' : ($index % 2 == 0 ? 'bg-success' : ($index % 1 == 0 ? 'bg-warning' : ''))}}">Thanh toán</button>
+                    </div>
+                </div>
+            </a>
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
