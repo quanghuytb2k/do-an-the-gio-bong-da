2f00c766197d1980336868cf6bbce8895fd08191
sytruong2001 <vanyyy2001@gmail.com>
2024-01-11 21:16:24 +0700

    fix footer

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -175,7 +175,7 @@
                     <div class="service-item rounded overflow-hidden">
                         <div class="service-item-img">
                             <a href="{{ route('detail-pitches',$item->id)}}" class="img-link">
-                                <img src="{{ asset($item->images)}}" alt="">
+                                <img src="{{ asset($item->images)}}" alt="" style="object-fit: contain">
                             </a>
                         </div>
                         <div class="position-relative p-4 pt-4">
@@ -192,7 +192,7 @@
     </div>
     <!-- Service End -->
     <!-- Projects Start -->
-    <div class="container-xxl py-5" style="height:100vh">
+    <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
                 <h1 class="mb-4">Các sản phẩm nổi bật</h1>
@@ -209,7 +209,7 @@
                     </ul>
                 </div>
             </div>
-            <div class="row g-4 portfolio-container wow fadeInUp" data-wow-delay="0.5s">
+            <div class="row mt-n2 wow fadeInUp" data-wow-delay="0.5s">
                 @foreach($product as $item)
                 <div class="col-lg-4 col-md-6 portfolio-item ">
                     <div class="portfolio-img rounded">
@@ -230,7 +230,7 @@
 
 
     <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-body mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container-fluid bg-dark text-body footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
